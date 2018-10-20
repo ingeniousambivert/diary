80ff89ef972b6d07596371f5c2344196d1257a3e
Monarch Maisuriya <37824914+ingeniousambivert@users.noreply.github.com>
2018-10-20 14:47:20 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+    
+$link = mysqli_connect("localhost", "monarch", "maisuriya", "monarch");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
