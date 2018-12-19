a2e64a79225b32e4c4f8aaedce9ffbae2d51e47d
Monarch Maisuriya <37824914+ingeniousambivert@users.noreply.github.com>
2018-12-19 13:11:37 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php
 
-    $link = mysqli_connect("shareddb1b.hosting.stackcp.net", "cl29-secretdi", "D-fnT^Hbz", "cl29-secretdi");
-        
+$link = mysqli_connect("localhost","root","maisuriya","diary");
         if (mysqli_connect_error()) {
             
             die ("Database Connection Error");
